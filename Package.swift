c046430e2201bc04620a694eefc181d65c1619cc
Junfeng Zhang <jeffzx15o@gmail.com>
2020-12-10 20:49:52 +0800

    Update Package.swift and add RxViewModelProtocol impl.

--- Package.swift
+++ Package.swift
@@ -5,24 +5,47 @@ import PackageDescription
 
 let package = Package(
     name: "Books",
+    platforms: [
+        .iOS(.v10),
+        .macOS(.v10_12)
+    ],
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
-        .library(
-            name: "Books",
-            targets: ["Books"]),
+        // Products define the executables and libraries produced by a package, and make them visible to other packages.
+        .library(name: "Books", targets: ["Books"])
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/Alamofire/Alamofire.git", from: "5.4.0"),
+        .package(url: "https://github.com/SnapKit/SnapKit.git", from: "5.0.1"),
+        .package(url: "https://github.com/Moya/Moya.git", from: "14.0.0"),
+        .package(url: "https://github.com/ReactiveX/RxSwift.git", from: "5.1.1"),
+        .package(url: "https://github.com/RxSwiftCommunity/RxDataSources.git", from: "4.0.1"),
+        .package(url: "https://github.com/onevcat/Kingfisher.git", from: "5.15.8")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
+        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "Books",
-            dependencies: []),
+            dependencies: [
+                .product(name: "Alamofire", package: "Alamofire"),
+                .product(name: "SnapKit", package: "SnapKit"),
+                .product(name: "Moya", package: "Moya"),
+                .product(name: "RxSwift", package: "RxSwift"),
+                .product(name: "RxCocoa", package: "RxSwift"),
+                .product(name: "RxRelay", package: "RxSwift"),
+                .product(name: "RxDataSources", package: "RxDataSources"),
+                .product(name: "Kingfisher", package: "Kingfisher")
+            ]
+        ),
         .testTarget(
             name: "BooksTests",
-            dependencies: ["Books"]),
-    ]
+            dependencies: [
+                "Books",
+                .product(name: "RxBlocking", package: "RxSwift"),
+                .product(name: "RxTest", package: "RxSwift"),
+            ]
+        )
+    ],
+    swiftLanguageVersions: [.v5]
 )
--- Sources/Books/Books.swift
+++ Sources/Books/Books.swift
@@ -1,3 +1,7 @@
-struct Books {
-    var text = "Hello, World!"
-}
+@_exported import RxSwift
+@_exported import RxCocoa
+@_exported import RxRelay
+@_exported import RxDataSources
+@_exported import SnapKit
+@_exported import Kingfisher
+@_exported import Moya
