01e0d6d5ad0d3a72c46a55b7f1d7ffbe99f9e72d
Junfeng Zhang <dev@akii.me>
2020-12-12 15:12:10 +0800

    Update Sidebar view.

--- Sources/Books/BookProtocol.swift
+++ Sources/Books/BookProtocol.swift
@@ -1,6 +1,6 @@
 import Foundation
 
-public protocol Model: Identifiable, Equatable, Hashable {}
+public protocol Model: Identifiable, Hashable {}
 
 public protocol BookProtocol: Model {
     var imageUrl: String? { get set }
