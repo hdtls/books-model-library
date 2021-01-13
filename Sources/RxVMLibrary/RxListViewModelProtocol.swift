ac5769e404e80ffdc7f7bfc2431f07d0a4248bff
Junfeng Zhang <dev@akii.me>
2021-01-13 13:10:13 +0800

    Delete reactive view-model implementation files.

--- Sources/RxVMLibrary/RxListViewModelProtocol.swift
+++
@@ -1,8 +0,0 @@
-import Foundation
-
-@available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 6.0, *)
-public protocol RxListViewModelProtocol
-: RxViewModelProtocol where Base: RandomAccessCollection {}
-
-open class RxListViewModel<Base>
-: RxViewModel<Base>, RxListViewModelProtocol where Base: RandomAccessCollection & Hashable {}
